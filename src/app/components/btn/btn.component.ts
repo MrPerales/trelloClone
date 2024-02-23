bc9eec93cb3b2ec3a33ac9f5f53b3ef55bca8a78
Carlos Perales <carlosperales050@gmail.com>
2024-02-22 19:43:57 -0600

    add: dinamic styles

--- src/app/components/btn/btn.component.ts
+++ src/app/components/btn/btn.component.ts
@@ -1,12 +1,28 @@
+import { CommonModule } from '@angular/common';
 import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-btn',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './btn.component.html',
 })
 export class BtnComponent {
   @Input() typeBtn: 'button' | 'submit' | 'reset' = 'button';
-  @Input() typeColor: 'success' | 'primary' = 'success';
+  @Input() color: 'success' | 'primary' | 'danger' = 'success';
+
+  //para que el color sea dinamico agregar en getColors
+  get getColors() {
+    return {
+      'bg-success-600 ': this.color === 'success',
+      'hover:bg-success-800': this.color === 'success',
+      'focus:ring-success-300': this.color === 'success',
+      'bg-primary-600 ': this.color === 'primary',
+      'hover:bg-primary-800': this.color === 'primary',
+      'focus:ring-primary-300': this.color === 'primary',
+      'bg-danger-600 ': this.color === 'danger',
+      'hover:bg-danger-800': this.color === 'danger',
+      'focus:ring-danger-300': this.color === 'danger',
+    };
+  }
 }
