89580686ebfb53de88817a9e9f794eb5625733f3
Carlos Perales <carlosperales050@gmail.com>
2024-02-26 13:29:16 -0600

    add: acordion (my workspace)

--- src/app/pages/boards/boards.component.ts
+++ src/app/pages/boards/boards.component.ts
@@ -1,17 +1,24 @@
 import { Component } from '@angular/core';
 import { NavbarComponent } from '../../components/navbar/navbar.component';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { CdkAccordionModule } from '@angular/cdk/accordion';
 import { faTrello } from '@fortawesome/free-brands-svg-icons';
 import {
+  faAngleDown,
+  faAngleUp,
+  faBorderAll,
   faBox,
   faClock,
+  faGear,
+  faHeart,
+  faUser,
   faWaveSquare,
 } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-boards',
   standalone: true,
-  imports: [NavbarComponent, FontAwesomeModule],
+  imports: [NavbarComponent, FontAwesomeModule, CdkAccordionModule],
   templateUrl: './boards.component.html',
 })
 export class BoardsComponent {
@@ -19,4 +26,10 @@ export class BoardsComponent {
   faBox = faBox;
   faWaveSquare = faWaveSquare;
   faClock = faClock;
+  faAngleUp = faAngleUp;
+  faAngleDown = faAngleDown;
+  faHeart = faHeart;
+  faBorderAll = faBorderAll;
+  faUsers = faUser;
+  faGear = faGear;
 }
