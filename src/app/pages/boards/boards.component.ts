d24b4cbc38897907e4ad7782b1523f77f44eb1e3
Carlos Perales <carlosperales050@gmail.com>
2024-02-26 14:01:28 -0600

    add:dinamic accordion

--- src/app/pages/boards/boards.component.ts
+++ src/app/pages/boards/boards.component.ts
@@ -32,4 +32,40 @@ export class BoardsComponent {
   faBorderAll = faBorderAll;
   faUsers = faUser;
   faGear = faGear;
+  // dinamic accordion
+  itemList = [
+    {
+      label: 'Item 1',
+      items: [
+        {
+          label: 'Sub Item 1.1',
+        },
+        {
+          label: 'Sub Item 1.2',
+        },
+      ],
+    },
+    {
+      label: 'Item 2',
+      items: [
+        {
+          label: 'Sub Item 2.1',
+        },
+      ],
+    },
+    {
+      label: 'Item 3',
+      items: [
+        {
+          label: 'Sub Item 3.1',
+        },
+        {
+          label: 'Sub Item 3.2',
+        },
+        {
+          label: 'Sub Item 3.3',
+        },
+      ],
+    },
+  ];
 }
