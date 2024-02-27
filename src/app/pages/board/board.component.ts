edc526ef1e1a5e40ce0f8b77d0a8b1cca68e9fdd
Carlos Perales <carlosperales050@gmail.com>
2024-02-26 19:53:37 -0600

    add : drag and drop transferencia entre colunmas

--- src/app/pages/board/board.component.ts
+++ src/app/pages/board/board.component.ts
@@ -3,6 +3,7 @@ import {
   CdkDragDrop,
   DragDropModule,
   moveItemInArray,
+  transferArrayItem,
 } from '@angular/cdk/drag-drop';
 import { NavbarComponent } from '../../components/navbar/navbar.component';
 import { toDO } from '../../models/toDo.Model';
@@ -29,21 +30,60 @@ export class BoardComponent {
   todos: toDO[] = [
     {
       id: '1',
-      title: 'Task 1',
+      title: 'buy a game',
     },
     {
       id: '2',
-      title: 'Task 2',
+      title: 'wathc a movie',
     },
     {
       id: '3',
-      title: 'Task 3',
+      title: 'Make dishes',
     },
   ];
-  drop(event: CdkDragDrop<any[]>) {
-    console.log(event);
-    // paara mover un item del array
-    // (array , previusIndex , currentIndex)
-    moveItemInArray(this.todos, event.previousIndex, event.currentIndex);
+  doing: toDO[] = [
+    {
+      id: '4',
+      title: 'play games',
+    },
+    {
+      id: '6',
+      title: 'coding',
+    },
+  ];
+  done: toDO[] = [
+    {
+      id: '5',
+      title: 'make homework',
+    },
+  ];
+  drop(event: CdkDragDrop<toDO[]>) {
+    // validacion para ver si el moviemiento es en la misma columna y
+    // hacemos reordenamiento
+    if (event.previousContainer === event.container) {
+      // paara mover un item del array
+      // event.container.data, se obtiene por la propiedad [cdkDropListData]
+      // del archivo HTML
+      // (array , previusIndex , currentIndex)
+      moveItemInArray(
+        event.container.data,
+        event.previousIndex,
+        event.currentIndex
+      );
+    } else {
+      //si es diferente container movermos entre contenedores
+      // transferArrayItem(
+      // datos del previus array
+      // datos actuales ,
+      // previousIndex,
+      // currentIndex
+      // )
+      transferArrayItem(
+        event.previousContainer.data,
+        event.container.data,
+        event.previousIndex,
+        event.currentIndex
+      );
+    }
   }
 }
