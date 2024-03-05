5386af2dcc5f91408d02998cd16810aa7d8f2e16
Carlos Perales <carlosperales050@gmail.com>
2024-03-05 13:41:01 -0600

    add: dinamic  styles

--- src/app/components/btn/btn.component.ts
+++ src/app/components/btn/btn.component.ts
@@ -9,28 +9,48 @@ import { Component, Input } from '@angular/core';
 })
 export class BtnComponent {
   @Input() typeBtn: 'button' | 'submit' | 'reset' = 'button';
-  @Input() color: 'success' | 'primary' | 'danger' | 'gray-light' = 'success';
+  @Input() color: 'success' | 'primary' | 'danger' | 'light' | 'sky' =
+    'success';
+  // mejorando el color del boton
+  mapColors = {
+    success: {
+      'bg-success-600 ': true,
+      'hover:bg-success-800': true,
+      'focus:ring-success-300': true,
+      'text-white': true,
+    },
+    primary: {
+      'bg-primary-600 ': true,
+      'hover:bg-primary-800': true,
+      'focus:ring-primary-300': true,
+      'text-white': true,
+    },
+    danger: {
+      'bg-danger-600 ': true,
+      'hover:bg-danger-800': true,
+      'focus:ring-danger-300': true,
+      'text-white': true,
+    },
+    light: {
+      'bg-gray-200 ': true,
+      'hover:bg-gray-500': true,
+      'focus:ring-gray-50': true,
+      'text-gray-700': true,
+    },
+    sky: {
+      'bg-sky-600 ': true,
+      'hover:bg-sky-800': true,
+      'focus:ring-sky-300': true,
+      'text-white': true,
+    },
+  };
 
   //para que el color sea dinamico agregar en getColors
   get getColors() {
-    return {
-      'text-white':
-        this.color === 'primary' ||
-        this.color === 'success' ||
-        this.color === 'danger',
-      'text-gray-700': this.color === 'gray-light',
-      'bg-success-600 ': this.color === 'success',
-      'hover:bg-success-800': this.color === 'success',
-      'focus:ring-success-300': this.color === 'success',
-      'bg-primary-600 ': this.color === 'primary',
-      'hover:bg-primary-800': this.color === 'primary',
-      'focus:ring-primary-300': this.color === 'primary',
-      'bg-danger-600 ': this.color === 'danger',
-      'hover:bg-danger-800': this.color === 'danger',
-      'focus:ring-danger-300': this.color === 'danger',
-      'bg-gray-200 ': this.color === 'gray-light',
-      'hover:bg-gray-500': this.color === 'gray-light',
-      'focus:ring-gray-50': this.color === 'gray-light',
-    };
+    const colors = this.mapColors[this.color];
+    if (colors) {
+      return colors;
+    }
+    return {};
   }
 }
