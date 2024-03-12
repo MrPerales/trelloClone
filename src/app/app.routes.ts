f74033d911bb0e72e604b62d68a188469bd143f5
Carlos Perales <carlosperales050@gmail.com>
2024-03-12 11:33:18 -0600

    add: routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,22 +1,60 @@
 import { Routes } from '@angular/router';
 import { LoginComponent } from './modules/auth/pages/login/login.component';
-import { BoardsComponent } from './modules/boards/pages/boards/boards.component';
-import { BoardComponent } from './modules/boards/pages/board/board.component';
 import { ScrollComponent } from './pages/scroll/scroll.component';
 import { TablesComponent } from './pages/tables/tables.component';
+import { ForgotPasswordComponent } from './modules/auth/pages/forgot-password/forgot-password.component';
+import { RegisterComponent } from './modules/auth/pages/register/register.component';
+import { RecoveryComponent } from './modules/auth/pages/recovery/recovery.component';
+import { LayoutComponent } from '../app/modules/layout/components/layout/layout.component';
+import { BoardsComponent } from '../app/modules/boards/pages/boards/boards.component';
+import { ProfileComponent } from './modules/profile/pages/profile/profile.component';
+import { UsersTableComponent } from '../app/modules/users/pages/users-table/users-table.component';
 
 export const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'login',
+    pathMatch: 'full',
+  },
   {
     path: 'login',
     component: LoginComponent,
   },
   {
-    path: 'boards',
-    component: BoardsComponent,
+    path: 'forgot-password',
+    component: ForgotPasswordComponent,
   },
   {
-    path: 'board',
-    component: BoardComponent,
+    path: 'register',
+    component: RegisterComponent,
+  },
+  {
+    path: 'recovery',
+    component: RecoveryComponent,
+  },
+  // *************Rutes /App **************************
+  {
+    path: 'app',
+    component: LayoutComponent,
+    children: [
+      {
+        path: '',
+        redirectTo: 'boards',
+        pathMatch: 'full',
+      },
+      {
+        path: 'boards',
+        component: BoardsComponent,
+      },
+      {
+        path: 'profile',
+        component: ProfileComponent,
+      },
+      {
+        path: 'users',
+        component: UsersTableComponent,
+      },
+    ],
   },
   {
     path: 'scroll',
--- src/app/modules/layout/components/layout/layout.component.ts
+++ src/app/modules/layout/components/layout/layout.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { NavbarComponent } from '../navbar/navbar.component';
+import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-layout',
   standalone: true,
-  imports: [NavbarComponent],
+  imports: [RouterOutlet, NavbarComponent],
   templateUrl: './layout.component.html',
 })
 export class LayoutComponent {}
