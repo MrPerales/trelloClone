96b32738d7d07bd73fed222dce1a0545b6f18db8
Carlos Perales <carlosperales050@gmail.com>
2024-03-14 18:37:39 -0600

    add: recovery password

--- src/app/modules/auth/components/forgot-password-form/forgot-password-form.component.ts
+++ src/app/modules/auth/components/forgot-password-form/forgot-password-form.component.ts
@@ -2,6 +2,8 @@ import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
 import { BtnComponent } from '../../../shared/components/btn/btn.component';
+import { AuthService } from '../../../../services/auth.service';
+import { RequestStatus } from '../../../../models/request-status.model';
 
 @Component({
   selector: 'app-forgot-password-form',
@@ -10,17 +12,31 @@ import { BtnComponent } from '../../../shared/components/btn/btn.component';
   templateUrl: './forgot-password-form.component.html',
 })
 export class ForgotPasswordFormComponent {
-  constructor(private formBuilder: FormBuilder) {}
+  constructor(
+    private formBuilder: FormBuilder,
+    private authService: AuthService
+  ) {}
+
   form = this.formBuilder.nonNullable.group({
     // nombre del campo : ['valor inicial', reglas de validacion]
     email: ['', [Validators.email, Validators.required]],
   });
-  status: string = 'init';
+  status: RequestStatus = 'init';
   emailSent = false;
   sendLink() {
     if (this.form.valid) {
       this.status = 'loading';
       const { email } = this.form.getRawValue();
+      this.authService.recovery(email).subscribe({
+        next: () => {
+          this.status = 'succes';
+          this.emailSent = true;
+        },
+        error: (error) => {
+          this.status = 'failed';
+          console.log(error);
+        },
+      });
     } else {
       this.form.markAllAsTouched();
     }
--- src/app/modules/auth/components/recovery-form/recovery-form.component.ts
+++ src/app/modules/auth/components/recovery-form/recovery-form.component.ts
@@ -4,6 +4,9 @@ import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { faEye, faEyeSlash } from '@fortawesome/free-solid-svg-icons';
 import { BtnComponent } from '../../../shared/components/btn/btn.component';
 import { CustomValidators } from '../../../../utils/validators';
+import { AuthService } from '../../../../services/auth.service';
+import { RequestStatus } from '../../../../models/request-status.model';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-recovery-form',
@@ -12,7 +15,23 @@ import { CustomValidators } from '../../../../utils/validators';
   templateUrl: './recovery-form.component.html',
 })
 export class RecoveryFormComponent {
-  constructor(private formBuilder: FormBuilder) {}
+  constructor(
+    private formBuilder: FormBuilder,
+    private authService: AuthService,
+    private route: ActivatedRoute,
+    private router: Router
+  ) {
+    this.route.queryParamMap.subscribe((params) => {
+      const token = params.get('token');
+      if (token) {
+        this.token = token;
+      } else {
+        // protect the route , if you dont have token
+        this.router.navigate(['/login']);
+      }
+    });
+  }
+
   form = this.formBuilder.nonNullable.group(
     {
       newPassword: ['', [Validators.minLength(6), Validators.required]],
@@ -24,14 +43,26 @@ export class RecoveryFormComponent {
       ],
     }
   );
-  status: string = 'init';
+  status: RequestStatus = 'init';
   faEye = faEye;
   faEyeSlash = faEyeSlash;
   showPassword = false;
+  token = '';
 
   recovery() {
     if (this.form.valid) {
-      // todo
+      this.status = 'loading';
+      const { newPassword, confirmPassword } = this.form.getRawValue();
+      this.authService.changePassword(this.token, newPassword).subscribe({
+        next: () => {
+          this.status = 'succes';
+          this.router.navigate(['/login']);
+        },
+        error: (error) => {
+          this.status = 'failed';
+          console.log(error);
+        },
+      });
     } else {
       this.form.markAllAsTouched();
     }
